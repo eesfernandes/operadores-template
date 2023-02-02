c22cd670e4ba19de8b5e11305337361f8822820c
eesfernandes <eesf@hotmail.com>
2023-02-01 22:39:22 -0300

    operadores-template

--- 01_pratica-guiada/script.js
+++ 01_pratica-guiada/script.js
@@ -1,1 +1,38 @@
-//CODIGO AQUI
+/*
+// Inicio do exercicio
+let receita = 15;
+let acrescimo = 22.40;
+let despesa = 12.34;
+let saldo = receita + acrescimo - despesa;
+console.log(`Tenho R$${receita.toFixed(2)} + Acrescimo R$${acrescimo.toFixed(2)} - Despesa R$${despesa.toFixed(2)} - Saldo R$${saldo.toFixed(2)}`);
+
+// Divisão entre irmãos
+let irmaos = saldo / 3
+console.log(`Ficou para cada irmão a quantia de: R$${irmaos.toFixed(2)}`);
+
+// Minha aplicação até ficar 3,42 vezes maior
+let minhaAplic = irmaos * 3.42
+console.log(`Minha parte pós aplicação: R$${minhaAplic.toFixed(2)}`);
+
+// Divisão do dinheiro em fatias inteiras entre 3 contas
+let tresContas = minhaAplic / 3;
+console.log(`Foi dividido o valor de: R$${minhaAplic.toFixed(2)} que resultou em: R$${tresContas.toFixed(2)} a ser depositado em cada uma das três contas.`);
+
+// Resto da divisão para a compra dos chicletes
+let restTresContas = minhaAplic % 3;
+console.log(`O resto da divisão das três contas para comprar chicletes foram no valor de: R$${restTresContas.toFixed(2)}`);
+
+// Igual e Diferente
+let a = 5 === 5;
+let b = 5 !== 5;
+let c = 5 !== '5';
+let d = '5' === 'cinco'
+let e = typeof 5 === typeof 20;
+let f = typeof '5' === typeof 'cinco';
+console.log(`A: ${a} - B: ${b} - C: ${c} - D: ${d} - E: ${e} - F: ${f}`);
+
+// Maior, Menor ou Igual
+let num1 = 5;
+let num2 = 3;
+console.log(`1 - ${num1 === num2} | 2 - ${num1 <= num2} | 3 - ${num1 > num2} | 4 - ${num1 < num2}`);
+*/
